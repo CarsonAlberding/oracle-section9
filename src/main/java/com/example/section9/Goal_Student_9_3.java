15fff39a898154ac9cbe2eff7818aa784e72b45a
Carson <carson.alberding@cdpipelinedevshop.com>
2022-08-30 10:22:45 -0400

    commit 2

--- src/main/java/com/example/section9/Goal_Student_9_3.java
+++ src/main/java/com/example/section9/Goal_Student_9_3.java
@@ -6,9 +6,23 @@ public class Goal_Student_9_3 {
         GoalTest_Student_9_3.root.getChildren().addAll();
         interactions();
     }
+    public void play_audio() {
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                    Main.class.getResourceAsStream("./Note5.wav" + url));
+            clip.open(inputStream);
+            clip.start();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}).start();
+        }
     
     private void interactions(){
         //Exercise 4   
-        
+        GoalTest_Student_9_3.root.setOnMouseClicked(play_audio);
+        GoalTest_Student_9_3.root.setOnMouseDragged(GoalTest_Student_9_3.root.getChildren(x,y));
     }
 }
